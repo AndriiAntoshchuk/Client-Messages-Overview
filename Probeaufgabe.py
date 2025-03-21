3659e0247a406316ee64e43a9385c7fed7b0a11d
andrii <ge92caq@mytum.de>
2025-03-21 13:32:36 +0100

    f

--- Probeaufgabe.py
+++ Probeaufgabe.py
@@ -4,11 +4,27 @@ import matplotlib.pyplot as plt
 # Datei laden
 data = pd.read_csv("data/raw/probeaufgabe_2025.csv")
 print(data.isnull().sum())
+data.info()
 
 # Spalte 'created_at' in Datetime-Format umwandeln
 data['created_at'] = pd.to_datetime(data['created_at'])
+# Neue Spalten hinzufügen
 data['weekday'] = data['created_at'].dt.day_name()
 data['hour'] = data['created_at'].dt.hour
+data = data.sort_values(by='created_at').reset_index(drop=True)
+start_date = data['created_at'].min()
+end_date = data['created_at'].max()
+print(f"Datenzeitraum: {start_date.date()} bis {end_date.date()}")
+
+# Fehlende Nachrichten basierend auf ihrer Kategorief nachfüllen
+category_filler = {
+    'technical_problem': 'Der Kunde hat ein technisches Problem.',
+    'contract_details': 'Der Kunde bittet um Details zu seinem Vertrag.',
+    'self_service': 'Der Kunde hat eine Frage zur Selbstbedienung.',
+    'termination': 'Der Kunde fragt nach Kündigungsoptionen.',
+    'payout': 'Der Kunde hat eine Frage zur Zahlung'
+}
+data['message'] = data.apply(lambda row: category_filler[row['category']] if pd.isnull(row['message']) else row['message'], axis=1)
 
 # Zeitliche Verteilung der Nachrichten
 plt.figure(figsize=(10, 5))
@@ -41,7 +57,7 @@ march_filtered = data[(data['created_at'].dt.month == 3) &
                       (data['created_at'].dt.day <= 17)]
 march_daily_category_counts = march_filtered.groupby([march_filtered['created_at'].dt.date, 'category']).size().unstack().fillna(0)
 march_daily_category_counts.plot(kind='bar', stacked=True, figsize=(12, 6), colormap='tab10')
-plt.xlabel("Tag (fortlaufende Nummer)")
+plt.xlabel("Tag")
 plt.ylabel("Anzahl der Nachrichten")
 plt.title("Häufigkeitsverteilung der Nachrichtenkategorien pro Tag (11. - 17. März)")
 plt.legend(title="Kategorie")
@@ -70,14 +86,7 @@ plt.xticks(rotation=0)
 plt.grid(axis="y")
 plt.show()
 
-category_filler = {
-    'technical_problem': 'Der Kunde hat ein technisches Problem.',
-    'contract_details': 'Der Kunde bittet um Details zu seinem Vertrag.',
-    'self_service': 'Der Kunde hat eine Frage zur Selbstbedienung.',
-    'termination': 'Der Kunde fragt nach Kündigungsoptionen.',
-    'payout': 'Der Kunde hat eine Frage zur Zahlung'
-}
-data['message'] = data.apply(lambda row: category_filler[row['category']] if pd.isnull(row['message']) else row['message'], axis=1)
+
 
 mean_messages = data['created_at'].dt.date.value_counts().mean()
 
@@ -86,5 +95,6 @@ messages_per_day = data['created_at'].dt.date.value_counts().sort_index()
 peak_day = messages_per_day.idxmax()
 peak_count = messages_per_day.max()
 print(f"Peak am {peak_day} mit {peak_count} Nachrichten (Durchschnitt: {mean_messages:.2f})")
+
 
 data.to_csv("data/processed/probeaufgabe_2025_filled_messages.csv", index=False)
